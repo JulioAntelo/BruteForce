3b5e1d65e3d6dc44df72536f17e354f9e8d987b6
Julio <jantram710@g.educaand.es>
2025-03-08 12:28:40 +0100

    completado con varios hilos

--- BruteForce/Program.cs
+++ BruteForce/Program.cs
@@ -11,13 +11,15 @@
         {
             string[] passwords = File.ReadAllLines("C:\\Users\\wenas\\RiderProjects\\BruteForce\\BruteForce\\passwords.txt");
             Random random = new Random();
+            
             string selectedPassword = passwords[random.Next(passwords.Length)];
-
+            
             string hashedPassword = ComputeSHA256Hash(selectedPassword);
             Console.WriteLine($"Hashed password: {hashedPassword}");
 
+            
             string crackedPassword = BruteForceAttack(passwords, hashedPassword);
-
+            
             if (crackedPassword != null)
             {
                 Console.WriteLine($"Password found: {crackedPassword}");
@@ -28,18 +30,23 @@
             }
         }
 
+        // Funcion para calcular el hash SHA256 de una cadena de texto
         static string ComputeSHA256Hash(string input)
         {
             using (SHA256 sha256 = SHA256.Create())
             {
+                // Convierte la cadena de texto en un array de bytes y calcula su hash
                 byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                // Convierte el array de bytes en una cadena hexadecimal y la devuelve
                 return BitConverter.ToString(bytes).Replace("-", "").ToLower();
             }
         }
 
+        // Funcion para realizar un ataque de fuerza bruta
         static string BruteForceAttack(string[] dictionary, string targetHash)
         {
             string foundPassword = null;
+          
             foreach (string password in dictionary)
             {
                 if (ComputeSHA256Hash(password) == targetHash)
@@ -48,9 +55,8 @@
                     break;
                 }
             }
-
+            
             return foundPassword;
         }
     }
 }
-
